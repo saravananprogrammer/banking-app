03a1f4e9adf16dfa77cc7533cbbd5cad8465d172
saravananprogrammer <saravanan.programnanalyst@gmail.com>
2022-05-30 14:12:26 +0530

    Devi task changes

--- src/app/gallery/gallery-list/gallery-list.component.ts
+++ src/app/gallery/gallery-list/gallery-list.component.ts
@@ -8,12 +8,43 @@ import { Component, OnInit,Input } from '@angular/core';
 export class GalleryListComponent implements OnInit {
  
   @Input() gallery :any
+
+  code :any =[];
+
+  
   constructor() { }
 
   ngOnInit(): void {
-    
+      console.log("gallery==>",this.gallery)
+
+      for(let i=0 ; i<this.gallery.length;i++){
+        let val = `
+        <div>
+        <div class="image">
+          <img src="../../assets/gallery/",${this.gallery[i]}>
+          <button class="remove" (click)="removeItem(i,$event)">X</button>
+        </div>
+      </div>
+     
+      `
+        this.code.push(val)
+      }
+
+
+
+
+
   }
-  removeItem(index:any){
+  removeItem(index:any,$event:any){
+
     this.gallery.splice(index,1)
+    this.code.splice(index,1)
   }
+
+  
+
+
+
+
+
 }
